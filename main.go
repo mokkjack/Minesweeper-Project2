9bd268b9e6ba5a872198edfd4bdf2a3ea1c5e1c1
Xavanion <barrettbrown2012@gmail.com>
2025-09-11 16:54:32 -0500

    Create foundation for creating/labeling grid

--- main.go
+++ main.go
@@ -2,13 +2,29 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
+	w := a.NewWindow("Grid Layout")
+  grid := container.New(layout.NewGridLayout(11))
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+  for row := 0; row < 11; row++{
+    for col := 0; col < 11; col++{
+      if row == 0 && col == 0 {
+
+      } else if row == 0{
+
+      } else if col == 0{
+
+      } else{
+        
+      }
+    }
+  }
+
+	w.SetContent(grid)
 	w.ShowAndRun()
 }

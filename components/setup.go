725a4a7a83f6b60914852d0f2e000a3e8596f355
BarrettBr <barrettbrown2012@gmail.com>
2025-09-21 21:47:49 -0500

    Comment on constant/setup.go

--- components/setup.go
+++ components/setup.go
@@ -6,7 +6,7 @@ Creation Date: 9/21/2025
 
 Description:
 - This file implements the setup screen for the game, where the user is able to choose the number of
-mines that they want in the game and validates that number if it's within the range.
+mines that they want in the game and validates that number if it's within the range. Afterwards it swaps the current view for the minesweeper view allowing the game to start
 
 Functions:
 - LoadSetupInfo: This loads the initial setup screen and asks the user for the number of mines.
@@ -16,7 +16,7 @@ Inputs:
 - Mine count from the user
 
 Outputs:
-- Either the Minesweeper board, or the error message depending on if the user entered a mine number in range or not.
+- Either the Minesweeper board, or the error message depending on if the user entered a mine number in range or not. (Error message will re-prompt for input)
 
 */
 
@@ -35,18 +35,23 @@ import (
 
 // LoadSetupInto sets the setup form as content of an existing window.
 // On submit it validates, builds the game UI, and replaces the window content.
+// Inputs: the fyne window itself
+// Outputs: Displays the window for the user
 func LoadSetupInto(win fyne.Window) {
 	entry := widget.NewEntry()
 	entry.SetPlaceHolder(fmt.Sprintf("Enter mine count (%d-%d)", config.MinMines, config.MaxMines))
-	entry.SetText("10")
+	entry.SetText(fmt.Sprintf("%d", config.MinMines))
 	errLabel := widget.NewLabel("")
 
+  // Create the "Setup window start"
 	start := widget.NewButton("Start Game", func() {
+    // Checks if entered value is int and not something random
 		n, err := strconv.Atoi(entry.Text)
 		if err != nil {
 			errLabel.SetText("Please enter a valid integer.")
 			return
 		}
+    // Bound checks
 		maxAllowed := config.BoardSize*config.BoardSize - 1
 		if n < config.MinMines || n > config.MaxMines {
 			errLabel.SetText(fmt.Sprintf("Mine count must be between %d and %d.", config.MinMines, config.MaxMines))
@@ -57,13 +62,14 @@ func LoadSetupInto(win fyne.Window) {
 			return
 		}
 
-		// Build the game UI and swap it in
+		// Build the minesweeper game UI and swap it in to work
 		h := NewGameHandler(n)
 		board := GetBoard(&h)
 		ui := SetupGameGraphics(board, &h)
 		win.SetContent(ui)
 	})
 
+  // Creates a vertical box and shows it to display the setup to the user
 	form := container.NewVBox(
 		widget.NewLabel(fmt.Sprintf("Select number of mines (%d-%d):", config.MinMines, config.MaxMines)),
 		entry,
--- config/constants.go
+++ config/constants.go
@@ -1,11 +1,31 @@
 package config
 
+/*
+Prologue
+
+Authors: Adam Berry, Barrett Brown, Jonathan Gott, Alex Phibbs, Minh Vu
+Creation Date: 9/11/2025
+
+Description:
+- This file hosts some basic config file bits to be called inside the go files instead of relying on magic numbers
+
+Functions:
+- None: Constant containing bunch of variables to be called
+
+
+Inputs:
+- None
+
+Outputs:
+- Int Value of the constant
+*/
+
 const (
-	BoardSize    = 10
-	MinMines     = 10
-	MaxMines     = 20
-	WindowHeight = 500
-	WindowWidth  = 500
-	FixedWinSize = true
-	GridSpacing  = (WindowHeight) / (BoardSize + 1)
+	BoardSize    = 10 // Used to declare how many mines in the board
+	MinMines     = 10 // Used to decide/display minimum allowed mines
+	MaxMines     = 20 // Used to decide/display maximum allowed mines
+	WindowHeight = 500 // Used to declare the window borders
+	WindowWidth  = 500 // Used to declare window borders
+	FixedWinSize = true // Bool to disallow adjusting window size
+	GridSpacing  = (WindowHeight) / (BoardSize + 1) // Used to adjust how much space in the grid
 )

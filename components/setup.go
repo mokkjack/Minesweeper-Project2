7dce3ffc8c956e8d0cd57636d95ae59059709d2a
minhvuw <minhtvuw@gmail.com>
2025-09-21 20:58:46 -0500

    added prologue for setup.go

--- components/setup.go
+++ components/setup.go
@@ -1,3 +1,25 @@
+/*
+Prologue
+
+Authors: Adam Berry, Barrett Brown, Jonathan Gott, Alex Phibbs, Minh Vu
+Creation Date: 9/11/2025
+
+Description:
+- This file implements the setup screen for the game, where the user is able to choose the number of
+mines that they want in the game and validates that number if it's within the range.
+
+Functions:
+- LoadSetupInfo: This loads the initial setup screen and asks the user for the number of mines.
+Upon a valid entry, it'll create a new game and replaces the window with the game board.
+
+Inputs:
+- Mine count from the user
+
+Outputs:
+- Either the Minesweeper board, or the error message depending on if the user entered a mine number in range or not.
+
+*/
+
 package components
 
 import (
@@ -41,7 +63,6 @@ func LoadSetupInto(win fyne.Window) {
 		ui := SetupGameGraphics(board, &h)
 		win.SetContent(ui)
 	})
-
 
 	form := container.NewVBox(
 		widget.NewLabel(fmt.Sprintf("Select number of mines (%d-%d):", config.MinMines, config.MaxMines)),
